b3b24b2eeb56eb2d547f5476a325ff9e81825abc
上帝の右手 <x_zhao11@163.com>
2017-07-24 17:05:03 +0800

    optimize python part

--- buildTable.sql
+++ buildTable.sql
@@ -1,4 +1,5 @@
 -- key [mul,uni,pri]
+create database if not exists test;
 use test;  
 -- 无索引表
 create table if not exists test1(   
@@ -7,26 +8,28 @@ birth date,
 id int NOT NULL,
 height int,
 weight int
-);  
+)engine = MyISAM;  
 
 -- 有索引表
 create table if not exists test2(   
 name varchar(20),
 birth date,
-id int NOT NULL,
+id int NOT NULL auto_increment,
 height int,
 weight int,
-index indexname(id)
-); 
+index (name),
+index (id)
+)engine = MyISAM; 
 
 -- 有唯一索引表
 create table if not exists test3(   
 name varchar(20),
 birth date,
-id int UNIQUE,
+id int NOT NULL UNIQUE auto_increment,
 height int,
-weight int
-);
+weight int,
+index (name)
+)engine = MyISAM;
 
 -- SELECT * INTO OUTFILE '/mnt/mysqlTest/data.txt'
 -- FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
--- python/testDemo.py
+++ python/testDemo.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 
 '''
-computer with 1cpu,4kernel,4G mem 
+computer with 1cpu,2kernel,4G mem 
 mysql table test1:
 +--------+-------------+------+-----+---------+-------+
 | Field  | Type        | Null | Key | Default | Extra |
@@ -83,15 +83,15 @@ def clear_table(tableName):
 
 # 每次insert一句
 @timeit
-def insertPerline(tableName,values):
+def insertPerline(tableName,values,mode='ignore'):
     num=len(values)
     try:
         conn=MySQLdb.connect(host='localhost',user='root',passwd='cloudera',db='test',port=3306)
         cur=conn.cursor()
 
         for i in range(num):
-            stmt1="insert into %s"%tableName
-            stmt=stmt1+"(name, birth, id, height, weight) values('%s','%s',%d,%d,%d)"%values[i]
+            stmt1="insert %s into %s"%(mode,tableName)
+            stmt=stmt1+" values('%s','%s',%d,%d,%d)"%values[i]
             cur.execute(stmt)
         conn.commit()
     except MySQLdb.Error,e:
@@ -101,13 +101,13 @@ def insertPerline(tableName,values):
 
 # 分批insert,优化
 @timeit
-def insertDiv(tableName,div,values):
+def insertDiv(tableName,div,values,mode='ignore'):
     num=len(values)
     try:
         conn=MySQLdb.connect(host='localhost',user='root',passwd='cloudera',db='test',port=3306)
         cur=conn.cursor()
         # mysqldb的占位符统一写为%s,区别于python
-        stmt1="insert into %s(name, birth, id, height, weight)"%tableName 
+        stmt1="insert %s into %s"%(mode,tableName) 
         stmt=stmt1+" values(%s,%s,%s,%s,%s)"
         for i in range(div):
             # 该语句对insert做专门的优化,相当于执行一条insert: 
@@ -121,13 +121,13 @@ def insertDiv(tableName,div,values):
 
 # 一次性load
 @timeit
-def loadAll(tableName,mode='ignore'):
+def loadAll(tableName,mode='ignore',disable=False):
     try:
         conn=MySQLdb.connect(host='localhost',user='root',passwd='cloudera',db='test',port=3306)
         cur=conn.cursor()
         stmt=("load data infile '/mnt/mysqlTest/data.txt' %s into table %s" 
          " FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'"%(mode,tableName)
-        )
+        )            
         cur.execute(stmt)
         conn.commit()
     except MySQLdb.Error,e:
@@ -140,21 +140,32 @@ def loadAll(tableName,mode='ignore'):
 if __name__ == '__main__':
     tableName=['test1','test2','test3']
     
-    # file=open('../data.txt')
-    # lines=file.readlines()
-    # file.close()
-    # values=[]
+    file=open('../data.txt')
+    lines=file.readlines()
+    file.close()
+    values=[]
 
-    # for line in lines:
-    #     l=line.rstrip('\n').split(',')
-    #     ll=(l[0][1:-1],l[1][1:-1],int(l[2]),int(l[3]),int(l[4]))
-    #     values.append(ll)
-    
-    clear_table(tableName[0])
+    for line in lines:
+        l=line.rstrip('\n').split(',')
+        ll=(l[0][1:-1],l[1][1:-1],int(l[2]),int(l[3]),int(l[4]))
+        values.append(ll)
+    for i in range(3):
+        clear_table(tableName[i])
+        insertPerline(tableName[i],values)
+
+        clear_table(tableName[i])
+        insertDiv(tableName[i],5,values)
+
+        clear_table(tableName[i])
+        loadAll(tableName[i])
+
+        clear_table(tableName[i])
+        loadAll(tableName[i],'replace')
+
+        print('\n')
     # insertDiv(tableName[0],5,values)
     # insertDiv(5,values,tableName)
     # loadAll(tableName[2],'ignore')
-    loadAll(tableName[0])
     # clear_table()
     # func_mysqldb2(values)
     # for i in [5,10,20,30,50,100,200]:
